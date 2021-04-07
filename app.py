0650345ee3ef6c7b7a75c47488f28648cdd44fd4
rishi0629 <rishi0629@utexas.edu>
2021-04-07 15:10:10 -0500

    updated app.py with userinfo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import userinfo_database as ud
 
 app = Flask(__name__)
 
@@ -13,19 +14,35 @@ database={'nachi':'123','james':'aac','karthik':'asdsf'}
 def login():
     name1=request.form['username']
     pwd=request.form['password']
-    if name1 not in database:
-	    return render_template('login.html',info='Invalid User')
-    else:
-        if database[name1]!=pwd:
-            return render_template('login.html',info='Invalid Password')
-        else:
-	         return render_template('home.html',name=name1)
+
+    valid = ud.isUserInfoCorrect(name1, pwd)
+    if(valid = 'Found'):
+        return render_template('home.html', name = name1)
+
+    elif(valid = 'Password'):
+        return render_template('login.html', info = 'Invalid Password')
+    else: 
+        return render_template('login.html', info = 'Invalid Username')
+    
+    # if name1 not in database:
+	#     return render_template('login.html',info='Invalid User')
+    # else:
+    #     if database[name1]!=pwd:
+    #         return render_template('login.html',info='Invalid Password')
+    #     else:
+	#          return render_template('home.html',name=name1)
 
 
 @app.route('/form_signup',methods=['POST','GET'])
 def signup():
     name1=request.form['username']
     pwd=request.form['password']
+
+    if (ud.isUsernameTaken(name1)):
+        return render_template('login.html', info='Username Taken')
+    else:
+        ud.addNewUser(name1, pwd)
+
 
 
 if __name__ == '__main__':
