1fa2c17b021ea398cc26bf578207cbc100dd02f2
sarahorsak <45637628+sarahorsak@users.noreply.github.com>
2021-04-07 16:23:27 -0500

    Delete application directory

--- application/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired,Email
-
-class LoginForm(FlaskForm):
-    email = StringField('Email',validators=[DataRequired(),Email()])
-    password = StringField('Password',validators=[DataRequired()])
-    submit = SubmitField('Login')
--- application/models.py
+++
@@ -1,7 +0,0 @@
-import flask
-from application import db
-
-
-class User(db.Document):
-    email = db.StringField()
-    password = db.StringField()
